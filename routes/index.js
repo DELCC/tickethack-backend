e32a27786cc5de1a833aae97efcbb6ada74a9d62
Clément DELCOURT <clm.delcourt@gmail.com>
2025-09-23 15:54:50 +0200

    [Fix] GET Trips

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var moment = require('moment');
 const Trip = require('../models/trips');
 
-router.get('/trips',(req,res) =>{
+router.get(`/trips/:departure/:arrival/:date`,(req,res) =>{
    /* req.body.departure = document.querySelector('#').value;
     req.body.arrival = document.querySelector('#').value;
     req.body.date = document.querySelector('#').value;*/
 
 
 Trip.find({
-  departure : req.body.departure, 
-  arrival : req.body.arrival,
-  date : { $gte: moment(req.body.date).startOf('day').format(), $lte: moment(req.body.date).endOf('day').format() } })
+  departure : req.params.departure, 
+  arrival : req.params.arrival,
+  date : { $gte: moment(req.params.date).startOf('day').format(), $lte: moment(req.params.date).endOf('day').format() } })
 .then(data => {
     res.json({trips : data});
 });
--- routes/reservations.js
+++ routes/reservations.js
@@ -1,5 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const Reservation = require('../models/reservations');
+
+router.post('/:id',(req,res)=>{
+    const newReservation = new Reservation({
+        trip_id : req.params.id,
+        status : {
+            isBooked : true,
+            isPurchased : false,
+            },
+    });
+
+    newReservation.save();
+    res.json({ Reservation : newReservation});
+});
+
+router.delete('/:id',(req,res)=>{
+    
+    Reservation.deleteOne({_id :ObjectID(req.params.id)})
+});
+
+
 
 module.exports = router;
